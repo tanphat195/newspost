5516341fe85f68501ffb7f7adf843cd08433b6f2
tanphat <phatnguyen.itc.k13@gmail.com>
2019-12-12 08:37:19 +0700

    init commit

--- android/app/src/main/java/com/newspost/MainApplication.java
+++ android/app/src/main/java/com/newspost/MainApplication.java
@@ -3,6 +3,8 @@ package com.newspost;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -45,6 +47,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new MapsPackage(),
+            new AsyncStoragePackage(),
             new RNCameraPackage(),
             new AsyncStoragePackage(),
             new VectorIconsPackage(),
